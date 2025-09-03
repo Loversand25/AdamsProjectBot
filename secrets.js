a7c4a03cf0ea680baf209e8f27d58e967ad75c5e
Loversand25 <promiseokonkwo96@gmail.com>
2025-09-03 22:23:04 +0100

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,8 +1,8 @@
 module.exports = {
-  TELEGRAM_BOT_TOKEN: '7018583070:AAFWtNov9SO504Xci2p7ImwgoUWv7-O36k0',
-  NETLIFY_AUTH_TOKEN: 'nfp_Bgwr6LPfemn6AAo6cR141yz6Xm64yEk4a003',
-  FIREBASE_CONFIG: require('./firebase-key.json'),
-  ADMIN_TELEGRAM_ID: '5906009940',
+  TELEGRAM_BOT_TOKEN: 
+  NETLIFY_AUTH_TOKEN: '
+  FIREBASE_CONFIG: require('),
+  ADMIN_TELEGRAM_ID: '
   OPAY_PAYMENT: {
     accountNumber: '8119412226',
     bank: 'Opay',
